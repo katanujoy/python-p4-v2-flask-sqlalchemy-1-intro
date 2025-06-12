dbff782bce9618a07dc8dffb03ca3cb6af89454e
katanujoy <katanujoyy99@gmail.com>
2025-06-12 13:01:26 +0300

    Set up Flask app with SQLAlchemy and Migrate; define initial Pet model

--- server/models.py
+++ server/models.py
@@ -1,14 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
 metadata = MetaData()
-
-# create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
-
-# define a model class by inheriting from db.Model.
-
 
 class Pet(db.Model):
     __tablename__ = 'pets'
